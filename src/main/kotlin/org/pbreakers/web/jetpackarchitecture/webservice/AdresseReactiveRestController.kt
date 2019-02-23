925ef15bca47ce9aab96c712528452335bb5686d
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 18:22:07 +0100

    Add AdresseReactiveRestController.kt

---
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/webservice/AdresseReactiveRestController.kt
@@ -0,0 +1,35 @@
+package org.pbreakers.web.jetpackarchitecture.webservice
+
+import org.pbreakers.web.jetpackarchitecture.database.entity.Adresse
+import org.pbreakers.web.jetpackarchitecture.database.repository.AdresseReactiveRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@RestController
+class AdresseReactiveRestController : HttpMethodController<Adresse> {
+
+    @Autowired
+    private lateinit var adresseReactiveRepository: AdresseReactiveRepository
+
+    @PostMapping("adresses")
+    override fun add(@RequestBody item: Adresse) {
+        adresseReactiveRepository.insert(item)
+    }
+
+    @DeleteMapping("adresses/{id}")
+    override fun delete(@PathVariable("id") id: String) {
+        adresseReactiveRepository.deleteById(id)
+    }
+
+    @GetMapping("adresses")
+    override fun getAll(): Flux<Adresse> {
+        return adresseReactiveRepository.findAll()
+    }
+
+    @GetMapping("adresses/{id}")
+    override fun getOne(@PathVariable("id") id: String): Mono<Adresse> {
+        return adresseReactiveRepository.findById(id)
+    }
+}
