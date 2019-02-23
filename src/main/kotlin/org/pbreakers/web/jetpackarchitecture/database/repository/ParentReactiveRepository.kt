db92a3bf1fc985d923f914be91b12beb4c64ded2
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 14:11:50 +0100

    Update ParentRepository

--- src/main/kotlin/org/pbreakers/web/jetpackarchitecture/database/repository/ParentReactiveRepository.kt
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/database/repository/ParentReactiveRepository.kt
@@ -5,8 +5,4 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-interface ParentReactiveRepository : ReactiveMongoRepository<Parent, String> {
-    fun getParentBy(id: String): Mono<Parent>
-    fun getAll(): Flux<Parent>
-    fun insert(parent: Parent): Mono<Parent>
-}
+interface ParentReactiveRepository : ReactiveMongoRepository<Parent, String>
