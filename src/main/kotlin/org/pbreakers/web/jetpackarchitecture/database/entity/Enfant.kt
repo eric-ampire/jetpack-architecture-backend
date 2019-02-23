3f95868598763d770db6528861aa36818bf92a32
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 18:30:44 +0100

    When we save a child we have to specifie the parent

--- src/main/kotlin/org/pbreakers/web/jetpackarchitecture/database/entity/Enfant.kt
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/database/entity/Enfant.kt
@@ -1,12 +1,14 @@
 package org.pbreakers.web.jetpackarchitecture.database.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 
 
 @Document
 class Enfant(
     @Id val id: String,
+    @DBRef val parent: Parent,
     val nom: String,
     val prenom: String
 )
