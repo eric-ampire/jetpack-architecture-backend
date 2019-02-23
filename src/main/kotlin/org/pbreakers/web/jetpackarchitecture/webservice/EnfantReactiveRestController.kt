8f5074f5406bbecc14711dac24d79d4f4396ae43
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 18:21:06 +0100

    Update EnfantReactiveRestController

--- src/main/kotlin/org/pbreakers/web/jetpackarchitecture/webservice/EnfantReactiveRestController.kt
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/webservice/EnfantReactiveRestController.kt
@@ -3,25 +3,32 @@ package org.pbreakers.web.jetpackarchitecture.webservice
 import org.pbreakers.web.jetpackarchitecture.database.entity.Enfant
 import org.pbreakers.web.jetpackarchitecture.database.repository.EnfantReactiveRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
-class EnfantReactiveRestController {
+class EnfantReactiveRestController : HttpMethodController<Enfant> {
 
-    @Autowired
-    private lateinit var enfantReactiveRepository: EnfantReactiveRepository
+    @Autowired private lateinit var enfantReactiveRepository: EnfantReactiveRepository
 
-    @GetMapping("enfants")
-    fun getAll(): Flux<Enfant> {
-        return enfantReactiveRepository.findAll()
+    @PostMapping(value = "enfants")
+    override fun add(@RequestBody item: Enfant) {
+        enfantReactiveRepository.insert(item)
     }
 
-    @PostMapping("enfants")
-    fun add(@RequestBody enfant: Enfant) {
-        enfantReactiveRepository.insert(enfant)
+    @DeleteMapping(value = "enfants/{id}")
+    override fun delete(@PathVariable("id") id: String) {
+        enfantReactiveRepository.deleteById(id)
+    }
+
+    @GetMapping("enfants/{id}")
+    override fun getOne(@PathVariable("id") id: String): Mono<Enfant> {
+        return enfantReactiveRepository.findById(id)
+    }
+
+    @GetMapping("enfants")
+    override fun getAll(): Flux<Enfant> {
+        return enfantReactiveRepository.findAll()
     }
 }
