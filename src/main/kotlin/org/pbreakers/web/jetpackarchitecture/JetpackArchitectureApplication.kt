38692845544422249784f5dea073fba0d2c396b1
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 13:20:06 +0100

    First commit

---
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/JetpackArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package org.pbreakers.web.jetpackarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JetpackArchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<JetpackArchitectureApplication>(*args)
+}
