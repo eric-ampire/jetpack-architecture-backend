54765cd96a9adc3543cc038c8c6e2fa089e03be1
Eric Ampire <ericampire.top@gmail.com>
2019-02-23 13:32:05 +0100

    Add entity

---
+++ src/main/kotlin/org/pbreakers/web/jetpackarchitecture/entity/Parent.kt
@@ -0,0 +1,15 @@
+package org.pbreakers.web.jetpackarchitecture.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Parent(
+    @Id val id: String,
+    @DBRef val adresse: Adresse,
+    @DBRef val enfants: List<Enfant>,
+    val nom: String,
+    val prenom: String,
+    val genre: Char
+)
